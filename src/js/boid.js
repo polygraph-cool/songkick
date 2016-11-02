f650b428ecfabd5de4660124610a4db192c03b0a
russellgoldenberg <russellgoldenberg@gmail.com>
2016-11-02 11:33:08 -0400

    restructure

---
+++ src/js/boid.js
@@ -0,0 +1,340 @@
+import vec2 from 'gl-matrix-vec2'
+
+// add limit function to vec library
+vec2.limit = function(out, v, high) {
+	let x = v[0]
+	let y = v[1]
+
+	let len = x * x + y * y
+
+	if (len > high * high && len > 0) {
+		out[0] = x
+		out[1] = y
+		vec2.normalize(out, out)
+		vec2.scale(out, out, high)
+	}
+
+	return out
+}
+
+const Boid = (opts) => {
+	let normalPoint
+	let predict = vec2.create()
+	let dir = vec2.create()
+
+	let a = vec2.create()
+	let b = vec2.create()
+	let ap = vec2.create()
+	let ab = vec2.create()
+	let clonea = vec2.create()
+	let predictLoc = vec2.create()
+	let followVec = vec2.create()
+
+	let accelerationVec = vec2.create()
+	let steerVec = vec2.create()
+	let diffVec = vec2.create()
+	let acceleration = vec2.create()
+
+	let location
+	let mass
+	let maxspeed
+	let maxforce
+	let radius
+	let velocity
+	let path
+
+	const setMass = (m) => {
+		mass = m
+		radius = m
+		maxspeed = Math.random() / m + 1
+		maxforce = m / 1 * 0.1
+	}
+
+	const setPath = (p) => {
+		path = p
+	}
+
+	const getLocation = () => {
+		return location
+	}
+
+	const getRadius = () => {
+		return radius
+	}
+
+  	const applyBehaviors = (boids) => {
+		// follow force
+		const f = follow(path)
+		// const separate force
+		const s = separate(boids)
+
+		/* Scale up forces to produce stronger impact */
+		vec2.scale(f, f, 1)
+		vec2.scale(s, s, 1.5)
+
+		/* Calculate the average force */
+		const forces = vec2.add(vec2.create(), f, s)
+
+		vec2.scale(forces, forces, 1 / mass)
+
+		/* Apply force */
+		applyForce(forces)
+	}
+	
+	const applyForce = (force) => {
+		vec2.add(acceleration, acceleration, force)
+  	}
+
+	const run = () => {
+		update()
+		// borders()
+		// render()
+	}
+
+	const update = () => {
+		/**
+		* New location = current location + (velocity + acceleration) limited by maximum speed
+		* Reset acceleration to avoid permanent increasing
+		*/
+		vec2.add(velocity, velocity, acceleration);
+		vec2.limit(velocity, velocity, maxspeed);
+		vec2.add(location, location, velocity);
+
+		accelerationVec.set([0, 0]);
+
+		acceleration = accelerationVec;
+	}
+
+   // TODO necessary?
+	// const borders = () => {
+	// 	if (location[0] < -radius) {
+	// 	location[0] = opts.width + radius;
+	// 	}
+	// 	if (location[0] > opts.width + radius) {
+	// 	location[0] = -radius;
+	// 	}
+	// }
+
+	/*
+	* @param {Object} path Instance of Path object including path points
+	*
+	* @returns {Array} Path following behavior
+	*/
+	const follow = () => {
+
+		/** Predict future location */
+		predict.set(velocity);
+
+		vec2.normalize(predict, predict);
+		vec2.scale(predict, predict, 25);
+
+		predictLoc.set([0, 0]);
+
+		vec2.add(predictLoc, predictLoc, location);
+		vec2.add(predictLoc, predictLoc, predict);
+
+		/** Define things */
+		var target = null;
+		var worldRecord = 1000000; // Will be updated with shortest distance to path. Start with a very high value.
+
+		/** Loop through each point of the path */
+		let i = path.points.length
+		while(i--) {
+		  /** Get current and next point of the path */
+		  a.set(path.points[i]);
+		  b.set(path.points[(i + 1) % path.points.length]);
+
+		  /** Calculate a normal point */
+		  var normalPoint = getNormalPoint(predictLoc, a, b);
+
+		  /** Calculate direction towards the next point */
+		  dir.set(b);
+
+		  vec2.sub(dir, dir, a);
+
+		  /**
+		   * Set a normal point to the end of the current path segment and
+		   * recalculate direction if the vehicle is not within it
+		   */
+		  if (normalPoint[0] < Math.min(a[0], b[0]) || normalPoint[0] > Math.max(a[0], b[0]) ||
+			  normalPoint[1] < Math.min(a[1], b[1]) || normalPoint[1] > Math.max(a[1], b[1])) {
+
+			normalPoint.set(b);
+
+			a.set(path.points[(i + 1) % path.points.length]);
+			b.set(path.points[(i + 2) % path.points.length]);
+
+			dir.set(b);
+			vec2.sub(dir, dir, a);
+		  }
+
+		  /** Get a distance between future location and normal point */
+		  var d = vec2.dist(predictLoc, normalPoint);
+
+		  /** Calculate steering target for current path segment if the vehicle is going in segment direction */
+		  if (d < worldRecord) {
+			worldRecord = d;
+			target = normalPoint;
+
+			vec2.normalize(dir, dir);
+			vec2.scale(dir, dir, 25);
+			vec2.add(target, target, dir);
+		  }
+		}
+
+		/**
+		 * Steer if the vehicle is out of the 1/5 of the path's radius
+		 * Do not steer otherwise
+		 *
+		 * Using a part of path's radius creates kind of non-straightforward movement
+		 * Instead of "in tube" movement when object bounces from path edges
+		 */
+		if (worldRecord > path.radius / 5) {
+		  return seek(target);
+		} else {
+		  followVec.set([0, 0]);
+
+		  return followVec;
+		}
+	}
+
+  /**
+   * Find normal point of the future location on current path segment
+   *
+   * @function
+   * @memberOf Boid
+   *
+   * @param {Array} p Future location of the vehicle
+   * @param {Array} a Start point of the path segment
+   * @param {Array} b End point of the path segment
+   *
+   * @returns {Array} Normal point vec2
+   */
+	const getNormalPoint = (p, a, b) => {
+		ap.set(p);
+		ab.set(b);
+
+		/** Perform scalar projection calculations */
+		vec2.sub(ap, ap, a);
+		vec2.sub(ab, ab, a);
+		vec2.normalize(ab, ab);
+		vec2.scale(ab, ab, vec2.dot(ap, ab));
+
+		clonea.set(a)
+		return vec2.add(vec2.create(), clonea, ab);
+	}
+
+  /**
+   * Produce path following behavior
+   *
+   * @function
+   * @memberOf Boid
+   *
+   * @param {Array} target Point on the path where vehicle is steering to
+   *
+   * @returns {Array} Path following behavior
+   */
+  const seek = (target) => {
+	vec2.sub(target, target, location)
+	return steer(target)
+  }
+
+  /**
+   * Check for nearby vehicles and produce steering away behavior
+   *
+   * @function
+   * @memberOf Boid
+   *
+   * @param {Array} boids A list of vehicles
+   *
+   * @returns {Array} Steering away/bouncing off behavior
+   */
+	const separate = (boids) => {
+		let desiredSeparation = radius * 2.25
+		let count = 0
+
+		steerVec.set([0, 0])
+
+		/** Loop through each vehicle */
+		let i = boids.length
+		while (i--) {
+			let other = boids[i]
+			let d = location
+
+			  /** Get distance between current and other vehicle */
+			  d = vec2.dist(d, other.getLocation());
+
+			  /** Do stuff if the vehicle is not current one and the other one is within specified distance */
+			  if ((d > 0) && (d < desiredSeparation)) {
+				var diff
+
+				diffVec.set([0, 0])
+
+				diff = vec2.sub(diffVec, location, other.getLocation()) // Point away from the vehicle
+
+				vec2.normalize(diff, diff)
+				vec2.scale(diff, diff, 1 / d) // The closer the other vehicle is, the more current one will flee and vice versa
+				vec2.add(steerVec, steerVec, diff)
+
+				count++
+			}
+		}
+
+		/** Get average steering vector */
+		if (count > 0) {
+		  vec2.scale(steerVec, steerVec, 1 / count)
+		}
+
+		/** Bounce! Steer away */
+		if (vec2.len(steerVec) > 0) {
+		  steer(steerVec)
+		}
+
+		return steerVec
+	}
+
+  /**
+   * Implement Craig Reynolds' steering algorithm
+   *
+   * @function
+   * @memberOf Boid
+   *
+   * @param {Array} target Point on the path or vector where vehicle is steering to
+   *
+   * @returns {Array} Steering behavior
+   */
+	const steer = (target) => {
+		var steer;
+
+		vec2.normalize(target, target);
+		vec2.scale(target, target, maxspeed);
+
+		steer = target;
+
+		vec2.sub(steer, steer, velocity);
+		vec2.limit(steer, steer, maxforce);
+
+		return steer;
+	}
+
+	const init = () => {
+		location = opts.location
+		velocity = vec2.fromValues(1, 1)
+
+		setMass(opts.mass)
+		setPath(opts.path)
+
+		return {
+			setMass,
+			setPath,
+			getLocation,
+			getRadius,
+			applyBehaviors,
+			run,
+		}
+	}
+
+	return init()
+}
+
+export default Boid
--- src/js/flock2.js
+++ src/js/flock2.js
@@ -1,6 +1,6 @@
 import vec2 from 'gl-matrix-vec2'
 import Path from './path'
-import Vehicle from './vehicle'
+import Boid from './boid'
 
 let clickCount = 0
 let time0 = Date.now()
@@ -49,94 +49,76 @@ const init = () => {
 	setupPath(path, 0.9)
 
 	const path2 = new Path(ctx)
-	setupPath(path2, 0.6)
+	setupPath(path2, 0.5)
 
 	const path3 = new Path(ctx)
-	setupPath(path3, 0.3)
+	setupPath(path3, 0.1)
 
-	/** Define vehicles list and push number of Vehicle object instances passing random location and mass */
-	var vehicles = [];
 
-	for (var i = 0; i < 500; i++) {
-		var mass = 2
+	const NUM_BOIDS = 300
+
+	const boids = d3.range(NUM_BOIDS).map(i => {
+		const mass = 2
 		const angle = Math.random() * Math.PI * 2
 		const x = Math.cos(angle) * WIDTH * 0.9 / 2 + WIDTH / 2
 	  	const y = Math.sin(angle) * HEIGHT * 0.9 / 2 + HEIGHT / 2
 	  	const location = vec2.fromValues(x, y)
-		var vehicle = new Vehicle({
+		return Boid({
 			location,
 			mass,
-			ctx,
-			width: WIDTH,
-			height: HEIGHT,
+			path,
 		})
-
-	  vehicles.push(vehicle);
-	}
+	})
 
 	/** Specify what to draw */
 	function draw() {
 
 	  /** Clear canvas */
-	  ctx.fillStyle = '#fff'
-	  ctx.fillRect(0, 0, WIDTH, HEIGHT)
+		ctx.fillStyle = '#fff'
+		ctx.fillRect(0, 0, WIDTH, HEIGHT)
 
-	  /** Render the path */
-	  // path.display();
-	  // path2.display();
-	  // path3.display();
+		/** Render the path */
+		// path.display()
+		// path2.display()
+		// path3.display()
 
-	  /**
-	   * Loop through each vehicle passing vehicles list and path to calculate things
-	   * Update and render vehicle
-	   */
-	  for (var i = 0; i < vehicles.length; i++) {
-	  	var p = path
-	  	var col = '#999'
-	  	var mass = 2
-	  	if (i < clickCount) {
-	  		p = path2
-	  		col = '#333'
-	  		mass = 3
-	  	} 
-	  	if (clickCount === 20 && i === 0) {
-	  		p = path3
-	  		col = '#a00'
-	  		mass = 5
-	  	}
-	    vehicles[i].applyBehaviors(vehicles, p, col, mass);
-	    vehicles[i].run();
-	  }
-	  	
+		let i = NUM_BOIDS
+		while (i--) {
+		    boids[i].applyBehaviors(boids)
+		    boids[i].run()
+		    
+		    // draw boid
+		    const loc = boids[i].getLocation()
+		    const r = boids[i].getRadius()
+			ctx.fillStyle = '#999'
+			ctx.beginPath()
+			ctx.arc(loc[0], loc[1], r, 0, Math.PI * 2, false)
+			ctx.closePath()
+			ctx.fill()
+		}
+		  	
 		time1 = Date.now()
 		const fpsVal = Math.round(1000 / (time1 - time0))
 		time0 = time1
 		fps.text(fpsVal)
 
-		requestAnimationFrame(draw);
+		requestAnimationFrame(draw)
 	}
 
 	/** Start simulation */
 	draw();
 
-	// /** Handle things appropriately onresize */
-	// function onResize() {
-	//   'use strict';
 
-	//   WIDTH = window.innerWidth;
-	//   HEIGHT = window.innerHeight;
-
-	//   canvas.width = WIDTH;
-	//   canvas.height = HEIGHT;
-
-	//   path.points = [];
-	//   setPoints();
-	// }
-
-	// window.addEventListener('resize', onResize, false);
 	const tick = () => {
 		clickCount++
-		if (clickCount < 20) setTimeout(tick, 1000)
+		if (clickCount < 10) {
+			boids[clickCount].setPath(path2)
+			boids[clickCount].setMass(4)
+			setTimeout(tick, 1000)
+		} else {
+			boids[1].setPath(path3)
+			boids[1].setMass(7)
+		}
 	}
 
 	setTimeout(tick, 4000)
