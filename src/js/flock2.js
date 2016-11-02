be28384bcd23408a79b9eee241e43428dab8fc0f
russellgoldenberg <russellgoldenberg@gmail.com>
2016-11-02 13:07:18 -0400

    temp

--- src/js/flock2.js
+++ src/js/flock2.js
@@ -8,7 +8,7 @@ let time1 = null
 const fps = d3.select('.fps')
 
 /** Init scene */
-const canvas = document.querySelector('.flock2')
+const canvas = document.querySelector('.flock2 canvas')
 const ctx = canvas.getContext('2d')
 
 let WIDTH = window.innerHeight * 0.8
@@ -21,6 +21,71 @@ canvas.width = WIDTH * 2
 canvas.height = HEIGHT * 2
 
 ctx.scale(2, 2)
+
+const setupText = () => {
+	const svg = d3.select('.flock2 svg')
+	//Create the SVG
+	svg
+		.attr("width", WIDTH)
+		.attr("height", HEIGHT)
+
+	const f = 0.9				
+	const startY = HEIGHT / 2
+	const rad = HEIGHT / 2 * f
+	const endY = HEIGHT / 2
+
+	const startX = WIDTH * (1 - f) + 13
+	const endX = WIDTH * f - 13
+
+	const startX2 = WIDTH * (1 - 0.5) + 13
+	const endX2 = WIDTH * 0.5 - 13
+
+	const startX3 = WIDTH * (1 - 0.1) + 13
+	const endX3 = WIDTH * 0.1 - 13
+
+	// svg.append('path')
+	// 	.attr('id', 'text-1')
+	// 	.attr('d', `M${startX},${startY} A${1},${1} 0 0,1 ${endX},${endY}`)
+	// 	.style('fill', 'none')
+	// 	.style('stroke', '#AAAAAA')
+	// 	.style('stroke-dasharray', '5,5')
+
+	// svg.append('text')
+	// 	.style('text-anchor','middle')
+	//   .append('textPath')
+	// 	.attr('xlink:href', '#text-1')
+	// 	.attr('startOffset', '50%')
+	// 	.text('Openers')
+
+	// svg.append('path')
+	// 	.attr('id', 'text-2')
+	// 	.attr('d', `M${startX2},${startY} A${1},${1} 0 0,1 ${endX2},${endY}`)
+	// 	.style('fill', 'none')
+	// 	.style('stroke', '#AAAAAA')
+	// 	.style('stroke-dasharray', '5,5')
+
+	// svg.append('text')
+	// 	.style('text-anchor','middle')
+	//   .append('textPath')
+	// 	.attr('xlink:href', '#text-2')
+	// 	.attr('startOffset', '50%')
+	// 	.text('Mid')
+
+	// svg.append('path')
+	// 	.attr('id', 'text-3')
+	// 	.attr('d', `M${startX3},${startY} A${1},${1} 0 0,1 ${endX3},${endY}`)
+	// 	.style('fill', 'none')
+	// 	.style('stroke', '#AAAAAA')
+	// 	.style('stroke-dasharray', '5,5')
+
+	// svg.append('text')
+	// 	.style('text-anchor','middle')
+	//   .append('textPath')
+	// 	.attr('xlink:href', '#text-3')
+	// 	.attr('startOffset', '50%')
+	// 	.text('Make it')
+
+}
 
 const setupPath = (path, factor) => {
 	/** Set path radius */
@@ -55,7 +120,7 @@ const init = () => {
 	setupPath(path3, 0.1)
 
 
-	const NUM_BOIDS = 300
+	const NUM_BOIDS = 400
 
 	const boids = d3.range(NUM_BOIDS).map(i => {
 		const mass = 2
@@ -74,8 +139,9 @@ const init = () => {
 	function draw() {
 
 	  /** Clear canvas */
-		ctx.fillStyle = '#fff'
-		ctx.fillRect(0, 0, WIDTH, HEIGHT)
+		// ctx.fillStyle = 'rgba(255,255,255,0.3)'
+		// ctx.fillRect(0, 0, WIDTH, HEIGHT)
+		ctx.clearRect(0, 0, WIDTH, HEIGHT)
 
 		/** Render the path */
 		// path.display()
@@ -90,11 +156,13 @@ const init = () => {
 		    // draw boid
 		    const loc = boids[i].getLocation()
 		    const r = boids[i].getRadius()
-			ctx.fillStyle = '#999'
+			ctx.fillStyle = 'rgba(0,0,0,0.2)'
+			// ctx.strokeStyle = 'rgba(0,0,0,0.2)'
 			ctx.beginPath()
 			ctx.arc(loc[0], loc[1], r, 0, Math.PI * 2, false)
 			ctx.closePath()
 			ctx.fill()
+			// ctx.stroke()
 		}
 		  	
 		time1 = Date.now()
@@ -125,6 +193,8 @@ const init = () => {
 	// window.addEventListener('click', () => {
 	// 	clickCount++
 	// })
+
+	setupText()
 }
 
 
