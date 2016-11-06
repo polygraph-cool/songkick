23cae1527c7b017c5dc09911ab8d58f7bc80ef93
russellgoldenberg <russellgoldenberg@gmail.com>
2016-11-06 12:19:53 -0500

    update dev version

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,6 +1,9 @@
 import flock from './flock'
+import ScrollMagic from 'scrollmagic'
+
 let ready = true
 const t = d3.select('.test')
+const m = d3.select('.magic')
 const test = () => {
 	t.text(window.scrollY)
 	ready = true
@@ -14,6 +17,18 @@ const init = () => {
 			requestAnimationFrame(test)
 		}
 	})
+	const controller = new ScrollMagic.Controller()
+	const scene = new ScrollMagic.Scene({
+			triggerElement: '#trigger1',
+			duration: 1000,
+		})
+		.on('enter', event => {
+			m.text(event.type)
+		})
+		.on('leave', event => {
+			m.text(event.type)
+		})
+		.addTo(controller)
 }
 
 export default { init }
