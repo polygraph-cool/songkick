8f20b1a79c9d88b2754f8d7d4f4882f2fe5ef937
russellgoldenberg <russellgoldenberg@gmail.com>
2016-11-10 16:50:41 -0500

    fetch data

---
+++ src/js/fetch-data.js
@@ -0,0 +1,39 @@
+import * as d3 from 'd3'
+
+function cleanVenues(row) {
+	return {
+		...row,
+		capacity: +row.capacity,
+	}
+}
+
+function loadVenues(cb) {
+	d3.csv('assets/data/web_venues.csv', cleanVenues, cb)
+}
+
+function cleanData(row) {
+	const order = row.order.split('|').map(d => +d)
+	const venue = row.venue.split('|')
+	const date = row.date.split('|')
+	return {
+		name: row.name,
+		shows: order.map((d, i) => ({
+			order: order[i],
+			venue: venue[i],
+			date: date[i],
+		}))
+	}
+}
+
+function loadData(cb) {
+	d3.csv('assets/data/web_data.csv', cleanData, cb)
+}
+
+function init(cb) {
+	d3.queue()
+		.defer(loadVenues)
+    	.defer(loadData)
+    	.awaitAll(cb)
+}
+
+export default init
--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,7 +1,15 @@
+import fetchData from './fetch-data'
 import flock from './flock'
 
 const init = () => {
-	flock.init()
+	fetchData((err, data) => {
+		const venues = data[0]
+		const bands = data[1]
+		console.log('his')
+	})
+	// flock.init()
+	// test
 }
 
 export default { init }
+
