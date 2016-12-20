dd73a5d40b02406724bb9b4568ee60dcf1dd39d0
russellgoldenberg <russellgoldenberg@gmail.com>
2016-12-20 17:27:39 -0500

    live

--- src/js/critical.js
+++ src/js/critical.js
@@ -1,7 +1,9 @@
 import loadFont from './utils/load-font'
 
 loadFont([
-	{ family: 'Libre Franklin', weight: 400 },
-	{ family: 'Libre Franklin', weight: 700 },
 	{ family: 'Libre Franklin', weight: 900 },
+	{ family: 'Bitter', weight: 700 },
+	{ family: 'Bitter', weight: 400 },
+	{ family: 'Libre Franklin', weight: 700 },
+	{ family: 'Libre Franklin', weight: 400 },
 ])
